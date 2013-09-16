2ef70daec35fe5a4eae481a852d9c61d9e2c4ede
Konstantinos Vaggelakos <kozze89@gmail.com>
2013-09-16 13:10:53 +0200

    Added imagemagick conversion on the uploads

--- routes/vectorize.js
+++ routes/vectorize.js
@@ -2,12 +2,13 @@
 # @@ScriptName: vectorize.js
 # @@Author: Konstantinos Vaggelakos<kozze89@gmail.com>
 # @@Create Date: 2013-09-11 15:10:01
-# @@Modify Date: 2013-09-11 17:59:09
+# @@Modify Date: 2013-09-16 13:08:44
 # @@Function:
 #*********************************************************/
 
 var fs = require('fs'),
   sh = require('execSync'),
+  im = require('imagemagick'),
   crypto = require('crypto');
 
 exports.upload = function(req, res){
@@ -22,17 +23,22 @@ exports.upload = function(req, res){
       fs.mkdirSync(newPath);
 
       // Write the original in that path
-      fs.writeFile(newPath + '/original.bmp', data, function (err) {
+      fs.writeFile(newPath + '/' + req.files.displayImage.name, data, function (err) {
         if (err) {
           log.error('Could not complete upload, since we could not write file');
         }
 
-        for (var i = 0; i < 10; i++) {
-          sh.run('potrace -o ' + newPath + '/' + i + '.svg' + ' -s -i -k ' + (i/10) + ' ' + newPath + '/original.bmp');
-        }
+        // First we convert it into bmp, since potrace works with bmp only
+        im.convert([newPath + '/' + req.files.displayImage.name, 'BMP3:' + newPath + '/original.bmp'], function(err, stdout) {
+          if (err) return console.error(err);
+          console.log('User uploaded non-bmp picture, that has now been converted');
 
-        res.redirect('/view/' + token);
+          for (var i = 0; i < 10; i++) {
+            sh.run('potrace -o ' + newPath + '/' + i + '.svg' + ' -s -i -k ' + (i/10) + ' ' + newPath + '/original.bmp');
+          }
 
+          res.redirect('/view/' + token);
+        });
       });
     });
   });
